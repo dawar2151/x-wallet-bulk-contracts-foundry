24a845a5dfb307e8f23cf2b6dfc88c9cd86ead8c
dawar2151 <zouhirtaousy@gmail.com>
2024-11-02 20:45:57 +0100

    add forge script

---
+++ script/BulkSender.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {BulkSender} from "../src/BulkSender.sol";
+
+contract BulkSenderDepoly is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address ownerAddress = vm.envAddress("OWNER_ADDRESS");
+        vm.startBroadcast(deployerPrivateKey);
+
+        BulkSender bulkSender = new BulkSender(ownerAddress);
+
+        vm.stopBroadcast();
+    }
+}
